5ff81720e3b4af2219f25652014d0ac348f9b4f8
JOLee83 <lee.justin.oliver@gmail.com>
2018-11-28 16:05:50 -0500

    predeploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,11 @@
-dotnet publish -c Release
+dotnet publish -c Release 
 
+cp dockerfile ./bin/release/netcoreapp2.1/publish
+
+docker build -t quicklee-api-image ./bin/release/netcoreapp2.1/publish
+
+docker tag quicklee-api-image  registry.heroku.com/quicklee/web
+
+docker push registry.heroku.com/quicklee/web
+
+heroku container:release web -a quicklee
